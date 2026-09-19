9c109ab2fab2568447ef8308852f0dd02636ffb9
thando544 <mpofu7085@gmail.com>
2026-09-19 23:36:01 +0200

    done with floating numbers in c

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h> //
 
 int main() { //without the main fucntion we can't execute a c programm
-    printf("hello");
 
+ //lesson 2 variables
+ //A variabke is a reusable container for a value
+
+
+//floating numbers
+//we use these for numbers that have floating/decimal numbers
+float price = 10.5;
+
+//when
+
+printf("sugar is %f. per unit\n", price);
     return 0; //you will always see the return  in a main function because it is expected to return an interger it returns it to operating system and zero means a sucess and non zero means an error
 }
