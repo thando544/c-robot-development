d998d50988334abcf8f2bd5ba8b7e18ec94ca77a
thando544 <mpofu7085@gmail.com>
2026-09-19 22:44:26 +0200

    first lesson closed and done

--- main.c
+++ main.c
@@ -2,4 +2,6 @@
 
 int main() { //without the main fucntion we can't execute a c programm
     printf("hello");
+
+    return 0; //you will always see the return  in a main function because it is expected to return an interger it returns it to operating system and zero means a sucess and non zero means an error
 }
